beb8b21d572ee1cddbb47e6e1931f22933d104f8
Oleksii_Konovalov <oleksii_konovalov@epam.com>
2024-01-17 13:13:42 +0200

    chore(test): Test EC2 script

--- scripts/petclinic_start.sh
+++ scripts/petclinic_start.sh
@@ -6,7 +6,7 @@ APP="petclinic"
 
 # Getting the environment name via meta data and ASG tag.
 # shellcheck disable=SC2046
-ENV=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$(curl -s https://169.254.169.254/latest/meta-data/instance-id)" --region $(curl -s https://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region) --query "Tags[?Key=='aws:autoscaling:groupName'].Value" --output text | cut -f2 -d"-")
+#ENV=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$(curl -s https://169.254.169.254/latest/meta-data/instance-id)" --region $(curl -s https://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region) --query "Tags[?Key=='aws:autoscaling:groupName'].Value" --output text | cut -f2 -d"-")
 
 # Running app with appropriate profiling group name.
 /usr/bin/java  -jar /usr/share/petclinic/petclinic.jar
